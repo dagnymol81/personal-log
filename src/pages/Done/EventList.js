45873bf27b364cc12e790504a2b89011da5bdbe3
Dagny Mol <dagnymol@gmail.com>
2022-12-17 12:00:35 -0500

    accordion for due, upcoming, all

--- src/pages/Done/EventList.js
+++ src/pages/Done/EventList.js
@@ -11,17 +11,97 @@ export default function EventList({ events }) {
   }
 
   const [eventsDue, setEventsDue] = useState(null)
+  const [upcomingEvents, setUpcomingEvents] = useState(null)
 
   useEffect(() => {
     if (events) {
       setEventsDue(events.filter(event => event.timeDue.toDate() < Date.now()))
+      setUpcomingEvents(events.filter(event => event.timeDue.toDate() > Date.now()))
     }
-    console.log(eventsDue)
-  }, [events])
+    
+  }, [events, eventsDue])
 
 
   return (
-    <div className="list">
+
+    <div className="container">
+
+<div className="accordion" id="accordionExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        It's time!
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+
+      {eventsDue && eventsDue.map(due => (
+      <>
+      <p>{due.event}&nbsp;
+      {formatDistanceToNow(due.timeDue.toDate(), { addSuffix: true })}&nbsp;
+      {due.tags && due.tags.map((tag, i) => (
+        <span key={i}>{tag.value} </span>
+      ))}
+      </p>
+      </>
+
+    ))}
+
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingTwo">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Coming Attractions
+      </button>
+    </h2>
+    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+
+      {upcomingEvents && upcomingEvents.map(coming => (
+      <>
+      <p>{coming.event}&nbsp;
+      {formatDistanceToNow(coming.timeDue.toDate(), { addSuffix: true })}&nbsp;
+      {coming.tags && coming.tags.map((tag, i) => (
+        <span key={i}>{tag.value} </span>
+      ))}
+      </p>
+      </>
+
+    ))}
+
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingThree">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Everything
+      </button>
+    </h2>
+    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+
+      {events && events.map(event => (
+      <>
+      <p>{event.event}&nbsp;
+      {formatDistanceToNow(event.timeDue.toDate(), { addSuffix: true })}&nbsp;
+      {event.tags && event.tags.map((tag, i) => (
+        <span key={i}>{tag.value} </span>
+      ))}
+      </p>
+      </>
+
+    ))}
+
+      </div>
+    </div>
+  </div>
+</div>
+
+    {/* <div className="list">
 
     <h3>Events Due</h3>
     {eventsDue && eventsDue.map(due => (
@@ -50,6 +130,7 @@ export default function EventList({ events }) {
       </div>
       ))}
 
+    </div> */}
     </div>
   )
 }
